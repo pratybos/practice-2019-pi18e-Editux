8260247f67046a1bfd675b6d11b091c512ae452f
Editux <43064329+Editux@users.noreply.github.com>
2019-06-04 08:20:00 +0300

    Create File reading.c

---
+++ File reading.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <dirent.h>
+
+#include <stdio.h>
+
+
+
+int main(void)
+
+{
+
+	DIR *d;
+
+	struct dirent *dir;
+
+	d = opendir(".");
+
+	if (d)
+
+	{
+
+		while ((dir = readdir(d)) != NULL)
+
+		{
+
+			printf("%s\n", dir->d_name);
+
+		}
+
+		closedir(d);
+
+	}
+	system("PAUSE");
+	return(0);
+
+}
